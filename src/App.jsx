264edf78b26bfe378ba0f5d8f46d1faa9f8a9e6e
TravisTLogan <TravisLoganT@gmail.com>
2023-06-21 23:58:11 +0100

    creation of basic files for react website

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experiance/Experiance'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
+// Initialise the App component is the root component of the application.
+const App = () => {
+  // render the components in a specified order.
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
