0701fbf2de228e41fa2700efe0b6cea9ecee1781
TravisTLogan <TravisLoganT@gmail.com>
2023-06-22 12:32:10 +0100

    added the display message

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,14 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    // display the introduction to the website
+    <header>
+      <div className="container header__container">
+      <h5>Hello I'm</h5>
+      <h1>Travis Logan</h1>
+      <h5 className="text-light">Fullstack Developer</h5>
+      </div>
+    </header>  
   )
 }
 
