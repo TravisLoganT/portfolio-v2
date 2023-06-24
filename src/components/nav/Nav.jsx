f6c2eafeedf298d53a12be5e5ff22684c54d13e6
TravisTLogan <TravisLoganT@gmail.com>
2023-06-24 14:43:43 +0100

    added icons for the nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './Nav.css'
+import {FaHome} from 'react-icons/fa'
+import {BiSolidHome, BiSolidUser, BiSolidBook, BiSolidMessageRounded} from 'react-icons/bi'
+import {MdComputer} from 'react-icons/md'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#'><BiSolidHome/></a>
+      <a href='#about'><BiSolidUser/></a>
+      <a href='#experience'><BiSolidBook/></a>
+      <a href='#services'><MdComputer/></a>
+      <a href='#contact'><BiSolidMessageRounded/></a>
+    </nav>
   )
 }
 
