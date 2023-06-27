72e4a806142536d19ab8313bf9ed8b54cd31f4c9
TravisTLogan <TravisLoganT@gmail.com>
2023-06-27 20:00:05 +0100

    started implementation of the projects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,24 @@ import './Portfolio.css'
 
 const Portfolio = () => {
   return (
-    <section id="portfolio">Portfolio</section>
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src="https://picsum.photos/500/300" alt="portfolio item" />
+          </div>
+
+          <h3>This a portfolio item title</h3>
+          <div className='portfolio__item-cta'>
+            <a href="#" className="btn" target='_blank'>Github</a>
+            <a href="#" className="btn btn--primary" target='_blank'>Demo</a>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
