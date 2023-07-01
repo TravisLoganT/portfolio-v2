6ec6515fb6779693b456b7b5c61da25c46232fe0
Travis <travislogant@gmail.com>
2023-07-01 23:55:54 +0100

    started formatting the testimonial with 1 review

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './Testimonials.css'
+import AVATAR1 from '../../assets/kamran.png'
+import AVATAR2 from '../../assets/kamran.png'
+import AVATAR3 from '../../assets/kamran.png'
+import AVATAR4 from '../../assets/kamran.png'
+import AVATAR5 from '../../assets/kamran.png'
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Review from Clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+        <article className='testimonial'>
+          <div className="client__avatar">
+            <img src={AVATAR1} alt="Kamran"></img>
+            <h5 className='client__name'>Kamran</h5>
+            <small className='client__review'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+            </small>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
