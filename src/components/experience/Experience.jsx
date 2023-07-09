8650d78fc74837ac33617b8cb811ed33099a71b4
Travis <travislogant@gmail.com>
2023-07-09 20:49:01 +0100

    added more experience

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -118,8 +118,133 @@ const Experience = () => {
                 <small className="text-light">Experiened</small>
               </div>
             </article>
+            
           </div>
         </div>
+
+        {/* Version Control Experience */}
+        <div className="experience__version">
+          <h3>Version Control</h3>
+
+          {/* experience 1 */}
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Git</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+
+            {/* experience 2 */}
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>GitHub & GitLab</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+
+            {/* experience 3 */}
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Command Line</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+
+            {/* experience 4 */}
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>CI/CD</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+
+            {/* experience 5 */}
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Documentation</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+
+        {/* Project Management Experience */}
+        <div className="experience__management">
+          <h3>Project Management</h3>
+
+          {/* experience 1 */}
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Agile</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+
+            {/* experience 2 */}
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Trello</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+
+            {/* experience 3 */}
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Slack</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+
+            {/* experience 4 */}
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Quality Assurance</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+
+            {/* experience 5 */}
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Communication</h4>
+                <small className="text-light">Experianced</small>
+              </div>
+            </article>
+
+            {/* experience 6 */}
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Leadership</h4>
+                <small className="text-light">Experianced</small>
+              </div>
+            </article>
+
+            {/* experience 7 */}
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Software Cycle</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+
+
       </div>
     </section>
   );
