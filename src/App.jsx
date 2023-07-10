42851e5ae0cc545ea9d1e175734adc9337e4c3c5
Travis <travislogant@gmail.com>
2023-07-10 16:16:23 +0100

    added the import of Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Analytics } from '@vercel/analytics/react';
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
@@ -23,6 +24,7 @@ const App = () => {
       <Testimonials />
       <Contact />
       <Footer />
+      <Analytics/>
     </>
   )
 }
