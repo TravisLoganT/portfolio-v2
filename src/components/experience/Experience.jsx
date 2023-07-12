70f46043a94255cfbe3c502de5624e192c880db1
Travis <travislogant@gmail.com>
2023-07-12 21:36:40 +0100

    typo fix

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -6,7 +6,7 @@ const Experience = () => {
   return (
     <section id="experience">
       <h5>What Skills I have</h5>
-      <h2>My Experiance</h2>
+      <h2>My Experience</h2>
 
       <div className="container experience__container">
         <div className="experience__frontend">
